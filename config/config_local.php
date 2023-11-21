a5baede339142242d3f14ecb30cafdb3e05a8171
spetrarca <67714780+spetrarca@users.noreply.github.com>
2023-11-20 22:16:25 -0500

    more cleanup

--- config/config_local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$databaseConfig = array(
-    'serverName' => 'siriusbphp-server.database.windows.net',
-    'database' => 'siriusbphp-database',
-    'username' => 'siriusbphp-server-admin',
-    'password' => '130G20WDUIH120QK$',
-);
-?>
